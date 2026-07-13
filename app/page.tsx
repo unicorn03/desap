f2ed68dbdcc026c650023d3520a211dc5e7feb99
hathoriastro <ahmadtovichha@gmail.com>
2026-07-13 16:39:44 +0700

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-white text-white">
-      <h1 className="text-7xl font-bold text-[#3e4d2b]">
-        Desa 
+    <div
+      className="flex min-h-screen flex-col items-center justify-center bg-cover bg-center bg-no-repeat text-white"
+      style={{
+        backgroundImage: "url('/backgrounddesa.jpg')",
+      }}
+    >
+      <h1 className="text-7xl text-center font-black text-[#3e4d2b] mb-4">
+        Desa
+        <br />
         Plumbangan
       </h1>
+
       <p className="mt-2 text-slate-400">
-        Next.js Environment + Tailwind CSS v3.4 Ready!
+        anjay lorem ipsum
       </p>
     </div>
-  )
+  );
 }
