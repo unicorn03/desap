1f07eabbe52d891ea2eb8a24676023e86b6b89c3
Ramadhan Cahaya <bimarama2014@gmail.com>
2026-07-13 16:30:23 +0700

    tebak 2

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"BgDAQCibT6XGkjRRWW16aE8v6TQE7RnnoVTW83cPWuo=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_1-7dtlk._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_next_dist_0dfntjm._.js","static/chunks/app_page_module_0d13ds3.css"],
-    source: "dynamic"
-});
