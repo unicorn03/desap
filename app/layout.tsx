a011700b731ed130de31235c344050cdedb57128
hathoriastro <ahmadtovichha@gmail.com>
2026-07-14 13:08:25 +0700

    beranda page v1 + react icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,33 @@
 import type { Metadata } from "next";
 import Navbar from "../components/Navbar";
 import "./globals.css";
+import { Outfit } from "next/font/google";
+import AboutSection from "@/app/Beranda/AboutSection";
+import VideoProfilSection from "./Beranda/VideoProfilSection";
+import Footer from "@/components/Footer";
+
 
 export const metadata: Metadata = {
   title: "Desa Plumbangan",
 };
 
+const outfit = Outfit({
+  subsets: ["latin"],
+});
+
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang="id">
-      <body>
-        <Navbar />
-        <main>{children}</main>
+    <html lang="en">
+      <body className={outfit.className}>
+        <Navbar/>
+        {children}
+        <AboutSection/>
+        <VideoProfilSection/>
+        <Footer/>
       </body>
     </html>
   );
